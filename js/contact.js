f1208c683a8535a1dda4d510b7e1617597c0a9b5
jason <jkinney23@yahoo.ca>
2018-05-14 16:19:45 +0300

    hide contact form at submission

---
+++ js/contact.js
@@ -0,0 +1,39 @@
+$.support.cors = true;
+$('#contact-form').submit(function(event){
+    event.preventDefault(); 
+    var data = {
+        "name": $('#name').val(),                 
+        "email": $('#email').val(),
+        "subject": $('#subject').val(),
+        "message": $('#message').val(),
+        "g-recaptcha-response":  $("#g-recaptcha-response").val()
+    }
+    if (data['g-recaptcha-response'].length == 0) {
+        alert('Please Confirm You Are a Human.');
+        return false;
+    }
+    else {
+        $.ajax(
+            {
+                type: 'POST',
+                url: 'https://mif71alpj2.execute-api.eu-west-1.amazonaws.com/email',                 
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                error: function(obj, msg, exc){
+                alert('Form Submission Failed.');
+                },
+                success: function(data){
+                $('.contact-content').fadeOut(200);
+                $('.submitted')
+                    .stop(true, false)
+                    .animate({
+                        height:"toggle",
+                        opacity:"toggle"
+                    }, 500);                      
+                }
+            } 
+        )
+    }
+    return false;
+});
